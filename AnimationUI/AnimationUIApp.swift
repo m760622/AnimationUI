10857b1a5c6794ba8f38d31dc74ec5aca8729844
Mohammed Abunada <m7606225@gmail.com>
2020-09-12 10:24:06 +0200

    Initial Commit

---
+++ AnimationUI/AnimationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationUIApp.swift
+//  AnimationUI
+//
+//  Created by Mohammed Abunada on 2020-09-12.
+//
+
+import SwiftUI
+
+@main
+struct AnimationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
